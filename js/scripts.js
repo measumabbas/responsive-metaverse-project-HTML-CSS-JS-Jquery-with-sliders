af086268e277a3eff43970e798e9880281b1cbad
= <=>
2022-11-23 00:08:11 +0500

    11 am

--- js/scripts.js
+++ js/scripts.js
@@ -85,6 +85,43 @@
 
         }
     });
+    var imageSliderThree = new Swiper('.image-slider-4', {
+        autoplay: {
+            delay: 4000,
+            disableOnInteraction: false
+		},
+        loop: true,
+        spaceBetween: 30,
+        slidesPerView: 1,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+            type: 'bullets',
+        },
+		breakpoints: {
+            // when window is <= 580px
+            580: {
+                slidesPerView: 1,
+                spaceBetween: 10
+            },
+            // when window is <= 768px
+            768: {
+                slidesPerView: 1,
+                spaceBetween: 20
+            },
+            // when window is <= 992px
+            992: {
+                slidesPerView: 2,
+                spaceBetween: 20
+            },
+            // when window is <= 1200px
+            1200: {
+                slidesPerView: 2,
+                spaceBetween: 20
+            },
+
+        }
+    });
 
 
    
