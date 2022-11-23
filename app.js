566e5702a03a6a875118428d95717cb71b92c495
= <=>
2022-11-23 21:30:31 +0500

    done

--- app.js
+++ app.js
@@ -12,3 +12,44 @@ menuBtn.addEventListener('click', () => {
 
   }
 });
+
+
+var imageSliderTwo = new Swiper('.image-slider-2', {
+//   autoplay: {
+//       delay: 2000,
+//       disableOnInteraction: false
+// },
+  loop: true,
+  spaceBetween: 30,
+  slidesPerView: 3,
+  pagination: {
+      el: '.offer-pagination',
+            clickable: true,
+        renderBullet: function (index, className) {
+          return `<span class="offer-pagination ${className}"></span>`;
+        },
+    },
+breakpoints: {
+      // when window is <= 580px
+      580: {
+          slidesPerView: 1,
+          spaceBetween: 10
+      },
+      // when window is <= 768px
+      768: {
+          slidesPerView: 1,
+          spaceBetween: 20
+      },
+      // when window is <= 992px
+      992: {
+          slidesPerView: 2,
+          spaceBetween: 20
+      },
+      // when window is <= 1200px
+      1200: {
+          slidesPerView: 2,
+          spaceBetween: 10
+      },
+
+  }
+});
--- js/scripts.js
+++ js/scripts.js
@@ -11,45 +11,7 @@
 	/* Preloader */
 
     /* Image Slider - Swiper */
-    var imageSliderTwo = new Swiper('.image-slider-2', {
-        autoplay: {
-            delay: 2000,
-            disableOnInteraction: false
-		},
-        loop: true,
-        spaceBetween: 30,
-        slidesPerView: 3,
-        pagination: {
-            el: '.swiper-pagination',
-                  clickable: true,
-              renderBullet: function (index, className) {
-                return '<span class="' + className + '"></span>';
-              },
-          },
-		breakpoints: {
-            // when window is <= 580px
-            580: {
-                slidesPerView: 1,
-                spaceBetween: 10
-            },
-            // when window is <= 768px
-            768: {
-                slidesPerView: 1,
-                spaceBetween: 20
-            },
-            // when window is <= 992px
-            992: {
-                slidesPerView: 2,
-                spaceBetween: 20
-            },
-            // when window is <= 1200px
-            1200: {
-                slidesPerView: 2,
-                spaceBetween: 10
-            },
-
-        }
-    });
+    
     var imageSliderThree = new Swiper('.image-slider-3', {
         autoplay: {
             delay: 2000,
@@ -59,7 +21,7 @@
         spaceBetween: 30,
         slidesPerView: 4,
         pagination: {
-            el: '.swiper-pagination-1',
+            el: '.swiper-pagination',
                   clickable: true,
               renderBullet: function (index, className) {
                 return '<div class="' + className + '"></div>';
