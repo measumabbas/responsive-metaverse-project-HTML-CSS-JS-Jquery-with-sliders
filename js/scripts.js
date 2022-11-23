dc05536436af20de455637a43f50cf2623f5a6a4
= <=>
2022-11-23 19:11:19 +0500

    before-pagination

--- js/scripts.js
+++ js/scripts.js
@@ -43,7 +43,7 @@
             // when window is <= 1200px
             1200: {
                 slidesPerView: 2,
-                spaceBetween: 20
+                spaceBetween: 10
             },
 
         }
@@ -65,21 +65,21 @@
             // when window is <= 580px
             580: {
                 slidesPerView: 1,
-                spaceBetween: 10
+                spaceBetween: 20
             },
             // when window is <= 768px
             768: {
-                slidesPerView: 1,
-                spaceBetween: 20
-            },
-            // when window is <= 992px
-            992: {
                 slidesPerView: 2,
                 spaceBetween: 20
             },
+            850: {
+                slidesPerView: 2,
+                spaceBetween: 20
+            },
+            
             // when window is <= 1200px
             1200: {
-                slidesPerView: 2,
+                slidesPerView: 3,
                 spaceBetween: 20
             },
 
@@ -98,29 +98,6 @@
             clickable: true,
             type: 'bullets',
         },
-		breakpoints: {
-            // when window is <= 580px
-            580: {
-                slidesPerView: 1,
-                spaceBetween: 10
-            },
-            // when window is <= 768px
-            768: {
-                slidesPerView: 1,
-                spaceBetween: 20
-            },
-            // when window is <= 992px
-            992: {
-                slidesPerView: 2,
-                spaceBetween: 20
-            },
-            // when window is <= 1200px
-            1200: {
-                slidesPerView: 2,
-                spaceBetween: 20
-            },
-
-        }
     });
 
 
